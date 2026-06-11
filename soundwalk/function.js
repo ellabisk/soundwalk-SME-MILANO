bb8c9cc36d9c4c78635bdea5f037e4d0a194ebcd
ellabisk <ellabisk@gmail.com>
2026-06-11 17:39:21 +0200

    Add files via upload

--- soundwalk/function.js
+++ soundwalk/function.js
@@ -4,7 +4,19 @@ function vaiMenu() {
 }
 
 function vaiPagina(num) {
+
+
     document.getElementById("menu").classList.add("hidden");
+    
+    if (num===1){///in questo modo facendo la stessa con ===agli altri numri si posso richiamare le pagine create dagli altri
+        creaPianoforte();
+} else{
+    document.getElementById("contenuto").classList.remove("hidden");
+
+    document.getElementById("titolo").innerText = "Pagina bottone " + num;
+}
+
+
     if (num===3){
         crea129()
 } else{
